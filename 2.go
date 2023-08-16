fc0b7da7945499a171bb79410ea7f01409352c4d
artemiikulikov <aapqwhat@gmail.com>
2023-08-16 17:11:50 +0300

    :star: update: add 2.go

---
+++ 2.go
@@ -0,0 +1,45 @@
+/* 
+Задача на распаковку
+
+Создать Go-функцию, осуществляющую примитивную распаковку строки, содержащую
+повторяющиеся символы/руны, например:
+"a4bc2d5e" => "aaaabccddddde"
+"abcd" => "abcd"
+"45" => "" (некорректная строка)
+"" => ""
+
+*/
+
+package main
+
+// import "strings"
+
+func f2(s string) string {
+  s2 := ""
+  // var v strings.Builder
+  var letter rune = 0
+  for _, val := range s {
+    if (int(val) - '0' >= 0 && int(val) - '0' <= 9 ) {
+      if (letter == 0) {
+        return ""
+      }
+      for i := 0; i < (int(val)-'0'); i++ {
+        s2 += string(letter)
+      }
+      letter = 1
+    } else {
+      if letter != 1 && letter != 0 {
+        s2 += string(letter)
+      }
+      letter = val
+    }
+  }
+  if letter == 0 {
+    return ""
+  }
+  if letter != 1 {
+        s2 += string(letter)
+  }
+
+  return s2
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vlle/cautious-octo-potato
+
+go 1.20
